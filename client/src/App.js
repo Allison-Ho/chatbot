960b6cd89dfb8097df6256e06c7299c13bb2b85a
qnho <qnho@uw.edu>
2024-06-12 09:45:09 -0700

    setup frontend

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './utils/variables.css';
+
+function App(){
+
+}
+
+export default App;
